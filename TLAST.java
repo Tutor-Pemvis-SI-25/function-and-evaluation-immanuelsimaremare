cbac2a9787bb511763c94bb694265d8afea01c44
immanuelsimaremare-max <immanuelsimaremare@gmail.com>
2025-11-21 09:32:56 +0700

    PAIR 10
    DONE

--- TLAST.java
+++ TLAST.java
@@ -1,7 +1,10 @@
+// 12S25058 - IMMANUEL SIMARE MARE
+// 12S25009 - ANGRINI NECY SIRAIT
+
 import java.util.*;
 import java.lang.Math;
 
-public class JavaApplication {
+public class TLAST {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
